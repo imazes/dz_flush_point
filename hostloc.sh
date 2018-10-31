9a0013f4c4db1c21a253250b6d8f93f9feb2d194
imazes <13517533+imazes@users.noreply.github.com>
2018-10-31 15:48:28 +0800

    Update hostloc.sh

--- hostloc.sh
+++ hostloc.sh
@@ -7,7 +7,7 @@ username=$1;
 password=$2;
 times=$3;
 cookie_name=$username'.hostloc.cookie';
-hostname='http://www.hostloc.com';
+hostname='https://www.hostloc.com';
 usa='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36';
 
 function random()
